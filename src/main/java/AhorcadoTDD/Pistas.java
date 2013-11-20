766c554c3be67b60f01d2658e985335732a8db9f
RudyEscalera <morbocuby@hotmail.com>
2013-11-20 13:50:52 -0300

    guardalo

--- src/main/java/AhorcadoTDD/Pistas.java
+++ src/main/java/AhorcadoTDD/Pistas.java
@@ -38,6 +38,36 @@ public class Pistas {
 		String resp = "cantidad de pistas es de 6";
 		return  resp;
 	}
+
+	public String NuevoArray(String palabraOculta, int posicionDeLaPista2,
+			String palabra,char letra) {
+		String resp=palabraOculta;
+		int cont=0;
+		if(BuscarLetraenPalabra(posicionDeLaPista2,palabra,letra)==true)
+		{
+			char[]auxiliar=resp.toCharArray();
+			auxiliar[posicionDeLaPista2]=letra;
+			resp= new String(auxiliar);
+		}
+		// TODO Auto-generated method stub
+		return resp;
+	}
+
+	public Boolean BuscarLetraenPalabra(int posicionDeLaPista2, String palabra,
+			char letra) {
+		boolean resp=false;
+		int cont=0;
+		while(cont!=palabra.length())
+		{
+			if(palabra.charAt(cont)==palabra.charAt(posicionDeLaPista2))
+				resp=true;
+			cont++;
+		}
+			
+		// TODO Auto-generated method stub
+		return resp;
+	}
+	
 	
 
 }
